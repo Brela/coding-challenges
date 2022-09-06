259fdd1305ee0eea71e6cbf27192843a658e78b5
Breland <claybreland1@gmail.com>
2022-09-05 23:34:14 -0500

    09-05-2022 adding items in a list with py

---
+++ adding_items_inalist.py
@@ -0,0 +1,7 @@
+# list - list of numbers, customers, files emails, blog posts, etc
+prices = [10, 20, 30, 876487, 98763287, 777484]
+
+total = 0
+for price in prices:
+    total += price
+    print(f"Total: {total}")
