9f3b571e04b1995ae3854672c1e8fac7ccd15f84
Breland <claybreland1@gmail.com>
2022-09-14 23:29:46 -0500

    09-14-2022 cool replies by py

---
+++ cool_replies_whilecommand_not_equaltoquit.py
@@ -0,0 +1,35 @@
+k = '>'
+while k != 'quit':
+    k = input('> ').lower()
+    if k == 'I love you':
+        print('I love you too')
+    elif k == 'whats up':
+        print('sup')
+    elif k == ('idk'):
+        print('idk either')
+    elif k == 'thanks':
+        print('No problem.')
+    elif k == "you're smart":
+        print('thanks!')
+    elif k == 'how old are you':
+        print('5')
+    elif k == 'you\'re good':
+        print('you\'re good too ')
+    elif k == ('whats your name'):
+        print('master slothy')
+    elif k == 'haha':
+        print('hahahahaha')
+    elif k == 'hi':
+        print('hello')
+    elif k == 'hi':
+        print('hey there')
+    elif k == 'hey':
+        print('howdy')
+    elif k == 'hello':
+        print('ello puppet')
+    elif k == ("whats my name"):
+        print('you know')
+    else:
+        print("i don't understand that yet")
+
+
