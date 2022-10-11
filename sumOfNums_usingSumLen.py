f52c69ab6cc7078ab66aa4a0647fe815bb580b0e
Breland <claybreland1@gmail.com>
2022-10-10 22:48:30 -0500

    10-10 sum of nums using len

---
+++ sumOfNums_usingSumLen.py
@@ -0,0 +1,8 @@
+
+numbers = [5, 3, 6, 100, 1, 59, 57]
+sumOfNums = sum(numbers)
+count = len(numbers)
+average = sumOfNums / count
+print("The list of numbers is:", numbers)
+print("The sum is:", sumOfNums)
+print("The average of all the numbers is:", average)
