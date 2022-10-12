036632a28dedab6a1c3e7dbe9bd9d0f8e88aa282
Breland <claybreland1@gmail.com>
2022-10-11 22:46:18 -0500

    10-11 sum of nums

---
+++ sumOfNums2.py
@@ -0,0 +1,7 @@
+
+numbers = [34, 22, 3321, 7722, 15567, 613, 361, 316, 7721]
+sumOfNums = sum(numbers)
+count = len(numbers)
+average = sumOfNums / count
+print("The list of numbers is:", numbers)
+print("The average of all the numbers is:", average)
