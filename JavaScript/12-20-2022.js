cb80ea6a36d7247df1bc9744eb80a4d3431f890a
Breland <claybreland1@gmail.com>
2022-12-20 07:59:53 -0600

    12-20-2022 fizzbuzz varient with reduce at end js

---
+++ JavaScript/12-20-2022.js
@@ -0,0 +1,33 @@
+//fizzbuzz variant
+/* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once. */
+
+// mine 
+function solution(num) {
+    let arr = []
+    if (num < 0) return 0
+    for (let i = 1; i < num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) arr.push(i)
+        else if (i % 5 === 0) arr.push(i)
+        else if (i % 3 === 0) arr.push(i)
+    }
+    return arr.reduce((curr, prev) => curr + prev, 0)
+}
+console.log(solution(20))
+// add to codewars page
+
+
+// best voted 
+function sol(number) {
+    var sum = 0;
+
+    for (var i = 1; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i
+        }
+    }
+    return sum;
+}
