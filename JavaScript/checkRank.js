3cb24f60765b4f0c350ca36ce1bc30d4a208cbcf
Breland <claybreland1@gmail.com>
2022-12-23 12:11:45 -0600

    use this code to check your rank in codeWars using devTools > console

---
+++ JavaScript/checkRank.js
@@ -0,0 +1,10 @@
+
+// this code can be used in console of codeWars to see your rank in a group
+// check the index of people with similar points to see oyour rank
+
+let list = Array.from(document.querySelectorAll('tr'))
+list = list.map(el => el = el.innerText)
+console.log(list)
+
+
+
