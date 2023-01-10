7c30cb57181d620e147876e5aad85aa1c279750e
Breland <claybreland1@gmail.com>
2023-01-10 10:27:13 -0600

    5 kyu - check if parenths set is valid

--- JavaScript/2023/01-02.js
+++ JavaScript/2023/01-02.js
@@ -2,7 +2,7 @@
 //  valid parentheses    5kyu
 
 /* Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. 
-he function should return true if the string is valid, and false if it's invalid.
+he function should return true if the string is valid, and false if it's invalid..
 
 Examples
 "()"              =>  true
