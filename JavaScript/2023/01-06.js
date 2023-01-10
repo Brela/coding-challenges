d94049b0aa041d7db2cbd2b3abccf8faa0d5d4e5
Breland <claybreland1@gmail.com>
2023-01-10 10:31:01 -0600

    5 kyu - how many 9s are in nums from 1 to n

--- JavaScript/2023/01-06.js
+++ JavaScript/2023/01-06.js
@@ -1,8 +1,5 @@
 /* 5kyu - how many 9's in count from 1 to n?
-
-It's 9 time!
-
-I want to count from 1 to n. How many times will I use a '9'?
+Counting from 1 to n. How many times will I use a '9'?
 
 9, 19, 91.. will contribute one '9' each, 99, 199, 919.. wil contribute two '9's each...etc
 
