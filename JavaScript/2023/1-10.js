9b44a783764fa0945b5478b4797b1f0e0fbf25a6
Breland <claybreland1@gmail.com>
2023-01-10 09:44:02 -0600

    7ku check letter count + 7kyu check for isogram

--- JavaScript/2023/1-10.js
+++ JavaScript/2023/1-10.js
@@ -1,3 +1,5 @@
+// 7kyu : count letters
+
 function count(str) {
     let sett = {}
 
@@ -8,3 +10,28 @@ function count(str) {
 }
 console.log(count('asdfaaa'))
 
+
+
+
+// 7kyu : check for isogram
+
+// mine
+
+function isIsogram(str) {
+    str = str.toLowerCase().split('')
+    let isogram = true;
+    console.log(str)
+    str.forEach((letter, i) => {
+        if ((str.filter(el => el === letter)).length > 1) isogram = false
+    })
+    return isogram;
+}
+
+console.log(isIsogram('abdf'))
+console.log(isIsogram('mravxudwqfcnzpgljnt'))
+
+
+// simplified solution
+function isIsogram1(str) {
+    return new Set(str.toUpperCase()).size == str.length;
+}
