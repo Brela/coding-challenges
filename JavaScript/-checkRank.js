93c4aebce0a25bda1cdee651700fd0e4d7830208
Brela <claybreland1@gmail.com>
2023-02-24 14:03:08 -0600

    let's goo

--- JavaScript/-checkRank.js
+++ JavaScript/-checkRank.js
@@ -1,6 +1,6 @@
 
-// this code can be used in console of codeWars to see your rank in a group.
-// check the index of people with similar points to see oyour rank.
+// this code can be used in console of codeWars to see your rank in a group
+// check the index of people with similar points to see oyour rank
 
 let list = Array.from(document.querySelectorAll('tr'))
 list = list.map(el => el = el.innerText)
