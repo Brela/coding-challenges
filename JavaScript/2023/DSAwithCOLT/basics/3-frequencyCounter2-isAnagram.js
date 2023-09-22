e095b9c3bd85c3f6310845b236162fd10c1c7d8f
Brela <claybreland1@gmail.com>
2023-09-22 09:12:04 -0500

    frequencycounter2

---
+++ JavaScript/2023/DSAwithCOLT/basics/3-frequencyCounter2-isAnagram.js
@@ -0,0 +1,38 @@
+// for time complexity to be O(n) we need three separate for loops
+// one to make an object out of the first
+// two to make an object out of the second
+// three to compare the values of the frist and second by this : for (let key in objOne) {} to access it and compare to object two values with the same key
+
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  // 2 empty objs
+  let objOne = {};
+  let objTwo = {};
+  // create an object with the first string with a loops
+  for (let char of str1) {
+    objOne[char] = objOne[char] + 1 || 1;
+  }
+  console.log(objOne);
+
+  // then do the same with the second string
+  for (let char of str2) {
+    objTwo[char] = objTwo[char] + 1 || 1;
+  }
+  console.log(objTwo);
+
+  // then compare the second to the first by checking the key of the first to the key of the second
+
+  for (let key in objOne) {
+    // console.log(objOne[key])
+
+    if (!objTwo[key]) return false;
+    if (objOne[key] !== objTwo[key]) return false;
+  }
+
+  // check if the first obj is empty
+
+  return true;
+}
+
+console.log(validAnagram("anagram", "nagaram"));
